e5d20cc868b2dc6d3ffbcb55a4e6fbf69327a94a
Muhammad Zuhri Hanifullah <zuhri2395@gmail.com>
2017-11-30 15:26:46 +0700

    Added nickname

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var connectedNick = {};
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
-	io.emit('chat message', 'a user connected');
-	
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+io.on('connection', function(client) {
+	client.on('join', function(name) {
+		connectedNick[client.id] = name;
+		io.emit('chatroom', name + ' joined the room');
 	});
 	
-	socket.on('disconnect', function() {
-		io.emit('chat message', 'a user disconnected');
+	client.on('send', function(msg) {
+		var name = connectedNick[client.id];
+		io.emit('chatroom', name + ': ' + msg);
 	});
+	
+	client.on('disconnect', function() {
+		io.emit('chatroom', connectedNick[client.id] + ' left the room');
+		delete connectedNick[client.id];
+	});
+	// io.emit('chat message', 'a user connected');
+	//
+	// socket.on('chat message', function(msg){
+	// 	io.emit('chat message', msg);
+	// });
+	//
+	// socket.on('disconnect', function() {
+	// 	io.emit('chat message', 'a user disconnected');
+	// });
 });
 
 http.listen(3000, function() {
